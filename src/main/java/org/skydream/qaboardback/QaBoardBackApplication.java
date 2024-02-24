40a43318cf1c9972ef359f852b32e17191f40ad3
김주현 <skydreamer@skyMac.local>
2024-02-24 13:32:11 +0900

    chore: project setting

---
+++ src/main/java/org/skydream/qaboardback/QaBoardBackApplication.java
@@ -0,0 +1,13 @@
+package org.skydream.qaboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaBoardBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaBoardBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skydream/qaboardback/QaBoardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skydream.qaboardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBoardBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
